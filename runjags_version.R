600e3b14439e50280747c7bcda8e2214a15139a6
shartn <44002974+shartn@users.noreply.github.com>
2021-07-13 21:09:12 +0200

    Add files via upload

---
+++ runjags_version.R
@@ -0,0 +1,62 @@
+## BLCM model is fitted using Markov Chain Monte Carlo sampling with JAGS (Just another Gibbs sampler)
+
+# Set working directory which should also include the model.bug file 
+setwd("C:\\Users\\shartn\\Documents\\Cost Harmony\\Cost WS 2021\\Trial files\\letter") 
+
+library('runjags')
+
+# Define data:
+N <- 110
+
+
+m.ca <-
+  structure(c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+              1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+              1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+              1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+              1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 
+              1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+              0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+              0), .Dim = c(110L, 3L), .Dimnames = list(NULL, c("PCR", "IGG", "IGM")))
+
+
+ones <- c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+          1, 1, 1, 1, 1, 1, 1, 1)
+
+# Set different starting values for the three different chains to assess convergence
+inits1 = list(".RNG.name" ="base::Mersenne-Twister",
+              ".RNG.seed" = 100022)
+inits2 = list(".RNG.name" ="base::Mersenne-Twister",
+              ".RNG.seed" = 300022)
+inits3 = list(".RNG.name" ="base::Mersenne-Twister",
+              ".RNG.seed" = 500022)
+
+# Run the model with the package runjags 
+results <- run.jags('model_final.bug', data=list(N=N, m.ca=m.ca, ones=ones), inits=list(inits1, inits2, inits3), 
+                    n.chains = 3, burnin = 10000, sample = 100000, adapt = 1000,
+                    monitor=c('prc','c1','c2','c3','s1','s2','s3','covs23'))
+
+# Model checking and diagnostics:
+# 1. visually checking the trace plots for mixing of the three chains
+# 2. checking the Gelman-Rubin convergence diagnostics,
+# the psrf (potential scale reduction factor) should be < 1.1
+results
+
+plot(results) 
+
+round(results$summary$quantiles,3)*100
+
